636533d2eacc3530cda89c689fcb6ea0324e3b4c
Moore <moorena@l200202.cs.jmu.edu>
2019-01-28 14:38:57 -0500

    added sentry code

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -3,13 +3,14 @@
 """ 
 SentryBot lets us know if an intruder walks past.
 
-Authors: Brandon Parr and Nathan Moore
-Version: 1.0
+Author: 
+Version:
 """
 
 import rospy
 
 from sensor_msgs.msg import Image
+from kobuki_msgs.msg import Sound
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
@@ -30,42 +31,42 @@ class SentryNode(object):
         """ Set up the Sentry node. """
         rospy.init_node('sentry')
         self.cv_bridge = CvBridge()
-        self.p = None
-        self.average = 1.0
         rospy.Subscriber('/camera/depth_registered/image',
                          Image, self.depth_callback, queue_size=1)
-        rospy.spin()
+	self.sound = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size=1)
+        
+	self.prev_scan = []
+	self.average = 0.0
+	self.alpha = 0
+	self.beep = Sound()
+	rospy.spin()
 
     def depth_callback(self, depth_msg):
         """ Handle depth callbacks. """
-
         # Convert the depth message to a numpy array
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
+	shape = depth[:, depth.shape[1] / 2]
+	
+	
+	if len(self.prev_scan) == 0:
+		self.prev_scan = shape
 
-        # YOUR CODE HERE.
-        # HELPER METHODS ARE GOOD.
-        middle = depth.shape[1] / 2
-        c = depth[:,middle]
-        c = c[~np.isnan(c)]
+	diff = self.prev_scan - shape
+	diff = np.abs(diff)
+	diff = diff[~np.isnan(diff)]
+	d = np.sum(diff)
 
-        d = 0
+	self.alpha = 0.85
+	self.average = (self.average * self.alpha) + d * (1 - self.alpha)
+	rospy.loginfo("AVERAGE = %f", self.average)
+	ratio = d / self.average
+	rospy.loginfo("ratio = %f", ratio)
+	if (ratio > 1.55):
+		self.beep.value = 0
+		self.sound.publish(self.beep)
 
-        if (self.p != None):
-        	d = np.sum(np.abs(c - self.p))
-
-        alpha = 0.5
-        self.average = (self.average * alpha) + (d * (1 - alpha))
-
-        temp = d / self.average
-        rospy.loginfo(temp)
-
-
-        self.p = c
-
-
-
-
-
-
+	# After you do things
+	self.prev_scan = shape
+	
 if __name__ == "__main__":
     SentryNode()
